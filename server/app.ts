13a1999500a3a64b42f30c51952a66c999537261
merkandez <131488754+merkandez@users.noreply.github.com>
2024-10-20 19:06:47 +0200

    feat: add npm install express @types/express, add database folder, add app.ts code

--- server/app.ts
+++ server/app.ts
@@ -1,30 +1,12 @@
-import express, { Application } from "express";
-import connectToMongoDB from "./database/db";
-import memeRoutes from "./routes/memeRoutes";
-import cors from 'cors';
+import { Router } from 'express';
+import { getPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/postController';
 
-export const app: Application = express();
-app.use(cors());
-const PORT = process.env.PORT || 8000;
+const router = Router();
 
-//middleware es para convertir json/js-js/json
-app.use(express.json());
+router.get('/', getPosts)
+router.get('/:id', getPostById);
+router.post('/', createPost);  
+router.put('/:id', updatePost);      
+router.delete('/:id', deletePost);   
 
-//para usar la ruta que queremos
-app.use("/api/memes", memeRoutes);
-
-const startServer = async () => {
-  try {
-    await connectToMongoDB();
-    console.log("👍 Connection has been established successfully.");
-  } catch (error) {
-    console.error("❌ Unable to connect to MongoDB", error);
-    throw error;
-  }
-};
-
-export const server = app.listen(PORT, () => {
-  console.log(`🏃‍♂️ Server running on http://localhost:${PORT}`);
-});
-
-startServer();
+export default router;
