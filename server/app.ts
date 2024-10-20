0665fc069c3325c9322cf2a57b99eec10862cd12
merkandez <131488754+merkandez@users.noreply.github.com>
2024-10-20 19:26:42 +0200

    feat: add npm i --save-dev @types/cors, change file name postRoutes

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,30 @@
-import { Router } from 'express';
-import { getPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/postController';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import sequelize from './database/connection_db'; // Conexión a la base de datos
+import postRoutes from './routes/postRoutes'; // Rutas de posts
 
-const router = Router();
+dotenv.config();
 
-router.get('/', getPosts)
-router.get('/:id', getPostById);
-router.post('/', createPost);  
-router.put('/:id', updatePost);      
-router.delete('/:id', deletePost);   
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-export default router;
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/posts', postRoutes); // Ruta base para las entradas de blog
+
+// Conexión a la base de datos
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa (￣y▽￣)╭ Ohohoho.....');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos (•ˋ _ ˊ•)', error);
+  });
+
+// Inicio del servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
