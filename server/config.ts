0388d81a473a1a5e381624a1d9c39e659e950a56
juamrm <ajulianaaamorim@gmail.com>
2024-10-22 09:53:38 +0200

    feat: add config.ts ajust

--- server/config.ts
+++ server/config.ts
@@ -10,7 +10,9 @@ const DB_TEST_NAME: string | undefined = process.env.DB_TEST_NAME;
 const NODE_ENV: string | undefined = process.env.NODE_ENV;
 
 if (!DB_DEV_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
-  throw new Error("⚠️  Missing environment variables to database connection ⚠️");
+  throw new Error(
+    "⚠️  Missing environment variables to database connection ⚠️"
+  );
 }
 
 export { DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_TEST_NAME, NODE_ENV };
