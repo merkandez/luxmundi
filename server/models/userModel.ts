9622c1d7e54572013faa2fb13898b4b82b719b8c
Monica <monicut3@gmail.com>
2024-10-22 14:24:29 +0200

    feat: add user model

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,2 +1,50 @@
-//import { DataTypes, Model } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../database/connection_db';
+import { IUser } from '../interfaces/IUsers';
+
+class User extends Model<IUser> implements IUser {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+// Definir el modelo User en Sequelize
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+    modelName: 'User',
+  }
+);
+
+export default User;
+
 
