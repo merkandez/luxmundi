880ac8b209864b962cac47265b7d837d70744f19
jennyfer85 <jmtg05@gmail.com>
2024-10-28 15:10:13 +0100

    feat: add components file.

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,37 @@
+<Section padding="1600">
+    <Panel gap="1200" type="half">
+        <FlexItem size="half">
+            <Flex direction="column" gap="600">
+                <TextContentHeading
+                    heading="Heading"
+                    subheading="Subheading"
+                />
+                <Text>
+                    Body text for your whole article or post.
+                    We’ll put in some lorem ipsum to show how a
+                    filled-out page might look:
+                </Text>
+                <Text>
+                    Excepteur efficient emerging, minim veniam
+                    anim aute carefully curated Ginza conversation
+                    exquisite perfect nostrud nisi intricate
+                    Content. Qui international first-class nulla
+                    ut. Punctual adipisicing, essential lovely
+                    queen tempor eiusmod irure. Exclusive izakaya
+                    charming Scandinavian impeccable aute quality
+                    of life soft power pariatur Melbourne occaecat
+                    discerning. Qui wardrobe aliquip, et Porter
+                    destination Toto remarkable officia Helsinki
+                    excepteur Basset hound. Zürich sleepy perfect
+                    consectetur.
+                </Text>
+            </Flex>
+        </FlexItem>
+        <Image
+            src={placeholder}
+            alt="Always use image alt"
+            aspectRatio="4-3"
+            size="medium"
+        />
+    </Panel>
+</Section>
