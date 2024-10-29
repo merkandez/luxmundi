4767e2ee6e48201960ed4289fa32f56107cfdbbc
Monica <monicut3@gmail.com>
2024-10-29 09:57:32 +0100

    feat: change folder structure

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,3 @@
+//import React from 'react';
+const LoginPage = () => <h1>Login Page</h1>;
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,3 @@
+//import React from 'react';
+const RegisterPage = () => <h1>Register Page</h1>;
+export default RegisterPage;
