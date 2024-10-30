b8acbc7051b32ac63a49ee3bffd5dffedb05ccc1
juamrm <ajulianaaamorim@gmail.com>
2024-10-30 12:32:34 +0100

    feat: review homepage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,11 @@
 import Header from "../components/Header";
-import { ButtonGroup } from "../components/ButtonGroup";
 import LuxMundiHero from "../components/LuxMundiHero";
-import HeroContent from "../components/HeroContent";
 
 function HomePage() {
   return (
     <div>
       <Header />
       <LuxMundiHero />
-      <ButtonGroup />
-      <HeroContent title="Lux Mundi" subtitle="algo muy guay aqui" />
     </div>
   );
 }
--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,23 +1,18 @@
-// src/router/AppRouter.jsx
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "../layout/Layout.jsx";
-import HomePage from "../pages/HomePage.jsx";
-import LoginPage from "../pages/LoginPage.jsx";
-import RegisterPage from "../pages/RegisterPage.jsx";
-import AboutUs from "../pages/AboutUs.jsx";
-import NotFound from "../pages/NotFound.jsx";
+// src/layout/Layout.jsx
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer"; // Example if you have a footer
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />, // Layout común para todas las rutas
-    errorElement: <NotFound />, // Página de error para rutas no encontradas
-    children: [
-      { index: true, element: <HomePage /> }, // Ruta raíz
-      { path: "login", element: <LoginPage /> }, // Ruta de login
-      { path: "register", element: <RegisterPage /> }, // Ruta de registro
-      { path: "aboutus", element: <AboutUs /> }, // Ruta de "About Us"
-    ],
-  },
-]);
-export default router;
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet /> {/* This is where child routes will render */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
