d0b40234c412c29f4cee23ad5cd25a0ad3b4fc4a
jennyfer85 <jmtg05@gmail.com>
2024-11-08 12:17:13 +0100

    feat: test cloudinary environment.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -6,4 +6,29 @@ export const getPosts = async () => {
     return response.data;
   };
 
- 
+// Cloudinary. 
+  export const subirImagenCloudinary = async (file) => {
+    // Convierte el archivo a Base64
+    const toBase64 = (file) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+  
+    const imageBase64 = await toBase64(file);
+  
+    try {
+      // Realiza la solicitud POST al backend con la imagen en formato Base64
+      const response = await axios.post('http://localhost:4000/upload', {
+        image: imageBase64,
+      });
+      // Devuelve la URL segura de la imagen subida
+      return response.data.secure_url;
+    } catch (error) {
+      console.error("Error subiendo la imagen:", error);
+      throw error;
+    }
+  };
+  
