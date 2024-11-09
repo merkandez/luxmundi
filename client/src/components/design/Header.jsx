3317367bf2fd46bef5f458d607286b01b31fd36a
merkandez <131488754+merkandez@users.noreply.github.com>
2024-11-09 18:13:08 +0100

    feat: add Header logic

--- client/src/components/design/Header.jsx
+++ client/src/components/design/Header.jsx
@@ -1,7 +1,7 @@
-// src/components/design/Header.jsx
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import AvatarIcon from '../design/AvatarIcon';
 
 const Header = ({
   isAuthenticated,
@@ -9,26 +9,63 @@ const Header = ({
   logout,
   openLoginModal,
   openRegisterModal,
-}) => (
-  <HeaderContainer>
-    <NavSection>
-      <StyledLink to='/'>Home</StyledLink>
-      <StyledLink to='/aboutus'>About Us</StyledLink>
-      {isAuthenticated ? (
-        <>
-          <StyledLink to='/profile'>Profile</StyledLink>
-          {role === 'admin' && <StyledLink to='/admin'>Admin Area</StyledLink>}
-          <LogoutButton onClick={logout}>Logout</LogoutButton>
-        </>
-      ) : (
-        <AuthButtonsContainer>
-          <Button onClick={openLoginModal}>Login</Button>
-          <Button onClick={openRegisterModal}>Register</Button>
-        </AuthButtonsContainer>
-      )}
-    </NavSection>
-  </HeaderContainer>
-);
+  avatarUrl,
+}) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => setMenuVisible((prev) => !prev);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <HeaderContainer>
+      <NavSection>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/aboutus">About Us</StyledLink>
+        
+        {isAuthenticated ? (
+          <>
+            {role === 'admin' && <StyledLink to="/admin">Admin Area</StyledLink>}
+
+            <ProfileContainer>
+              {/* Si avatarUrl está disponible, mostramos la imagen; de lo contrario, mostramos el AvatarIcon */}
+              {avatarUrl ? (
+                <AvatarImage onClick={toggleMenu} src={avatarUrl} alt="Avatar" />
+              ) : (
+                <AvatarIconWrapper onClick={toggleMenu}>
+                  <AvatarIcon />
+                </AvatarIconWrapper>
+              )}
+              {menuVisible && (
+                <ProfileMenu ref={menuRef}>
+                  <StyledLink to="/profile">Edit Profile</StyledLink>
+                  <LogoutButton onClick={logout}>Logout</LogoutButton>
+                </ProfileMenu>
+              )}
+            </ProfileContainer>
+          </>
+        ) : (
+          <AuthButtonsContainer>
+            <Button onClick={openLoginModal}>Login</Button>
+            <Button onClick={openRegisterModal}>Register</Button>
+          </AuthButtonsContainer>
+        )}
+      </NavSection>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
 
@@ -63,7 +100,7 @@ const AuthButtonsContainer = styled.div`
 `;
 
 const Button = styled.button`
-  background-color: #007bff; // Color del botón
+  background-color: #007bff;
   color: white;
   border: none;
   border-radius: 5px;
@@ -71,7 +108,7 @@ const Button = styled.button`
   cursor: pointer;
   font-weight: bold;
   &:hover {
-    background-color: #0056b3; // Color al pasar el cursor
+    background-color: #0056b3;
   }
 `;
 
@@ -82,5 +119,55 @@ const LogoutButton = styled.button`
   cursor: pointer;
   &:hover {
     text-decoration: underline;
+  }
+`;
+
+const ProfileContainer = styled.div`
+  margin-left: auto;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+const AvatarImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const AvatarIconWrapper = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const ProfileMenu = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background-color: #444;
+  border-radius: 5px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+
+  & a,
+  & button {
+    padding: 10px;
+    color: white;
+    text-decoration: none;
+    text-align: left;
+    width: 100%;
+    &:hover {
+      background-color: #555;
+    }
   }
 `;
