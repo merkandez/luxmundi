2360f35f5c5f34aedc1a6c400fb925070cb9df82
jennyfer85 <jmtg05@gmail.com>
2024-11-14 02:41:03 +0100

    feat: prepare dependencies for test

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -1,3 +1,27 @@
 import { rest } from 'msw'; 
 
-export const handelers
+export const handlers = [
+    rest.get('http://localhost:8080/api', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                users: [
+                    {
+                        id: 1,
+                        username: 'John Doe',
+                        email: 'john@example.com',
+                        password: 'password123',
+                        role: 'user',
+                    },
+                    {
+                        id: 2,
+                        username: 'user1',
+                        email: 'user1@example.com',
+                        password: '123456',
+                        role: 'admin',
+                    },
+                ],
+            })
+        );
+    }),
+]
